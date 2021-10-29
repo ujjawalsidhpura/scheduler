8ac0df64043710fe12a3e9d4a0838b23d1976022
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2021-10-29 19:13:31 +0530

    custom hook created, testing done on it and passed all tests

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
+export default function useVisualMode(initial) {
+
+  const [mode, setMode] = useState(initial)
+  const [history, setHistory] = useState([initial])
+
+  function transition(mode, replace = false) {
+
+    if (replace === true) {
+      back()
+      setMode(mode)
+    }
+
+    history.push(mode)
+    setMode(mode)
+
+  }
+
+  function back() {
+    //Back should not trigger if there is only one item in history
+    // i.e 'initial mode'
+    if (history.length > 1) {
+      history.pop()
+      const mode = history[history.length - 1]
+      setMode(mode)
+    }
+  }
+
+  return { mode, transition, back }
+}
